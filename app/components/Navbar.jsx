394e2b6dc89813cf0694a9064ec30992cc9a2a55
ShaileshParigi <shaileshparigi13@gmail.com>
2023-08-16 12:36:11 +0530

    First Commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Dojo HelpDesk</h1>
+      <Link href="/"> DashBoard</Link>
+      <Link href="/tickets"> Tickets</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/tickets/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <h2>Tickets</h2>
+    </main>
+  );
+};
+
+export default page;
