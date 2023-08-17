b307089f2d8279f3e5b6407f47d3f6a0c6ff628e
ShaileshParigi <shaileshparigi13@gmail.com>
2023-08-17 14:02:04 +0530

    Nav Logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Dojo HelpDesk Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo HelpDesk</h1>
       <Link href="/"> DashBoard</Link>
       <Link href="/tickets"> Tickets</Link>
