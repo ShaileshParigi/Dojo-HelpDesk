22756975e332da3a43922b2e3249a5ed1fd3014b
ShaileshParigi <shaileshparigi13@gmail.com>
2023-08-18 16:25:05 +0530

    Data Fetching Example

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import TicketList from "./ticketList";
 
 const page = () => {
   return (
     <main>
-      <h2>Tickets</h2>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open Tickets</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
     </main>
   );
 };
